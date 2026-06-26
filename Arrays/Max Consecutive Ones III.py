6a773cb6451fd28b5c3ea6441dfde3d0f52e4aac
monish18106 <monish180106@gmail.com>
2026-06-26 03:37:03 +0000

    max ones 3

--- Arrays/Max Consecutive Ones III.py
+++ Arrays/Max Consecutive Ones III.py
@@ -1,17 +1,16 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        m= 0 
         l = 0
-        r = 0
         z = 0
-        n = len(nums)
-        while l < n:
-            if nums[l] == 0:
+        mx = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
                 z += 1
-            m = max(m, abs(l-r+1))
             while z > k:
-                if a[r] == 0:
+                if nums[l] == 0:
                     z -= 1
-                r += 1
-            l += 1
-        return m
+                l += 1
+            mx = max(mx,r-l+1)
+
+
+        return mx
